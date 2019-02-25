69fc5e757de219f209981dff2c84cc867a35da19
Satheesh Prabhakaran Venkatasamy <ext.satheesh.v@deltatre.it>
2019-02-25 12:04:48 +0530

    Updated elasticsearchhealth.py

--- excercise2/elasticsearchhealth.py
+++ excercise2/elasticsearchhealth.py
@@ -1,17 +1,22 @@
-#Using library requests
+#Using library requests and os
 import requests
+import os.path
 
-#Getting IP of elasticsearch from ansible output file
-ip=open("publicip.txt").readline().rstrip()
+#Check if file exists
+if os.path.isfile("./publicip.txt"):
+    #Getting IP of elasticsearch from ansible output file
+    ip=open("publicip.txt").readline().rstrip()
 
-#Forming URL for elasticsearch health
-url="http://" + ip + ":9200/_cluster/health"
-
-#Requesting the elasticsearch health
-try:
-   request_output=requests.get(url, timeout=1)
-   eshealth=request_output.json()
-   print ("Cluster health of name " + eshealth["cluster_name"] + " is " + eshealth["status"].upper())
-
-except:
-   print ("Cluster with IP " + ip + " is not running")
+    #Forming URL for elasticsearch health
+    url="http://" + ip + ":9200/_cluster/health"
+    
+    #Requesting the elasticsearch health
+    try:
+       request_output=requests.get(url, timeout=1)
+       eshealth=request_output.json()
+       print ("Cluster health of name " + eshealth["cluster_name"] + " is " + eshealth["status"].upper())
+    
+    except:
+       print ("Cluster with IP " + ip + " is not running")
+else:
+    print ("VM has not been created. Run ansible playbook to create publicip.txt file.")
